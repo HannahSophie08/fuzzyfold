1617271e69882f66de6290f2abf0df04af5438df
Hannah Hartung <hannah.hartung@t-online.de>
2026-01-09 15:13:42 +0100

    Added macrostates as pairsets

--- crates/ff_kinetics/src/lib.rs
+++ crates/ff_kinetics/src/lib.rs
@@ -1,6 +1,9 @@
 pub mod timeline;
+pub mod timeline_pairsets;
 pub mod timeline_io;
+pub mod timeline_io_pairsets;
 pub mod timeline_plotting;
+pub mod timeline_plotting_pairsets;
 pub mod reaction;
 pub mod commit_and_delay;
 
@@ -8,8 +11,12 @@ mod rate_model;
 mod loop_structure;
 mod stochastic_simulation;
 mod macrostates;
+mod macrostates_pairsets;
 
 pub use rate_model::*;
 pub use loop_structure::*;
 pub use stochastic_simulation::*;
 pub use macrostates::*;
+pub use macrostates_pairsets::{Macrostate as MacrostatePT, MacrostateRegistry as MacrostateRegistryPT};
+
+
